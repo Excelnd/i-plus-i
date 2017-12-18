3227b7f36a66afdc7bb04e41977e298d05d08bb0
ihsan <ihsanmpm@gmail.com>
2017-12-18 10:21:12 +0530

    Add project files.

---
+++ i plus i/i plus i.cpp
@@ -0,0 +1,17 @@
+// i plus i.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int i = 10;
+int main()
+{
+	
+	int i = 20, c = 7;
+	std::cout << i +::i %c;
+	
+
+    return 0;
+}
+
